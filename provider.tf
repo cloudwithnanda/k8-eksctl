933b916f89cd9900ca40f7e5d0e9431c59bfe4f8
Nanda Kiran <royalbalabhadra@gmail.com>
2026-02-09 14:57:51 +0530

    adding scripts

--- provider.tf
+++ provider.tf
@@ -7,15 +7,15 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "nanda3sa"
+    bucket = "nanda-s3-2"
     key    = "eksctl"
-    region = "eu-west-2"
+    region = "us-east-1"
     dynamodb_table = "nandadb"
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
 }
 
